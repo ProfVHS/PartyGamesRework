8d1ac1a37b67635e92880281aaea1475d92596b2
Rafał <rubajrafal04@gmail.com>
2024-06-07 16:16:47 +0200

    animations for forms

--- client/src/components/layouts/RowLayout.tsx
+++ client/src/components/layouts/RowLayout.tsx
@@ -1,21 +1,24 @@
-import React from "react";
+import React from 'react';
 
 type RowLayoutProps = {
   children: React.ReactNode;
   gap?: number;
+  ref?: React.RefObject<HTMLDivElement>;
 };
 
-export const RowLayout = ({ children, gap }: RowLayoutProps) => {
+export const RowLayout = ({ children, gap, ref }: RowLayoutProps) => {
   return (
     <div
+      ref={ref}
       style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "row",
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row',
         gap: `${gap ?? 8}px`,
-        width: "100%",
-      }}>
+        width: '100%',
+      }}
+    >
       {children}
     </div>
   );
--- client/src/routes/Home/HomePage.tsx
+++ client/src/routes/Home/HomePage.tsx
@@ -3,38 +3,62 @@ import './styles.scss';
 import logo from '../../assets/textures/logo.svg';
 import { RowLayout } from '../../components/layouts/RowLayout';
 import { Button } from '../../components/Button/Button';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { CreateForm } from '../../components/Forms/CreateForm';
+import { useAnimate } from 'framer-motion';
 
 export const HomePage = () => {
+  const [scope, animate] = useAnimate();
   const [status, setStatus] = useState<'selecting' | 'join' | 'create'>(
     'selecting'
   );
+
+  const changeStatus = async (status: 'join' | 'create') => {
+    animate(
+      scope.current,
+      { scale: [1, 0.5], opacity: [1, 0] },
+      { duration: 0.5, type: 'spring' }
+    );
+    setStatus(status);
+  };
+
+  useEffect(() => {
+    animate(
+      scope.current,
+      { scale: [0.5, 1], opacity: [0, 1] },
+      { duration: 0.5, type: 'spring' }
+    );
+  }, [status]);
 
   return (
     <div className="home">
       <div className="home__content">
         <img src={logo} alt="logo" draggable={false} />
         <span className="home__title">Party Games</span>
-        {status === 'selecting' && (
-          <RowLayout>
-            <Button
-              style={{ width: '50%' }}
-              onClick={() => setStatus('create')}
-            >
-              Create Room
-            </Button>
-            <Button style={{ width: '50%' }} onClick={() => setStatus('join')}>
-              Join Room
-            </Button>
-          </RowLayout>
-        )}
-        {status === 'join' && (
-          <JoinForm onCancel={() => setStatus('selecting')} />
-        )}
-        {status === 'create' && (
-          <CreateForm onCancel={() => setStatus('selecting')} />
-        )}
+        <div className="home__forms" ref={scope}>
+          {status === 'selecting' && (
+            <RowLayout ref={scope}>
+              <Button
+                style={{ width: '50%' }}
+                onClick={() => changeStatus('create')}
+              >
+                Create Room
+              </Button>
+              <Button
+                style={{ width: '50%' }}
+                onClick={() => changeStatus('join')}
+              >
+                Join Room
+              </Button>
+            </RowLayout>
+          )}
+          {status === 'join' && (
+            <JoinForm onCancel={() => setStatus('selecting')} />
+          )}
+          {status === 'create' && (
+            <CreateForm onCancel={() => setStatus('selecting')} />
+          )}
+        </div>
       </div>
     </div>
   );
