07d93e6a55250a8fc98592f3aff1711a74ca7423
Oliwier <worekit@gmail.com>
2024-08-14 09:49:25 +0200

    score user functions

---
+++ server/Modules/User/Functions/updateScoreUser.ts
@@ -0,0 +1,19 @@
+import { db } from '../../../Database/database';
+
+export const updateScoreUser = async (id: string, score: number) => {
+  await new Promise<void>((resolve, reject) => {
+    db.run(
+      `UPDATE users SET score = ? WHERE id = ?`,
+      [score, id],
+      (err: Error) => {
+        if (err) {
+          console.error('updateScoreUser.ts: Update Score');
+          console.error(err.message);
+          reject();
+        } else {
+          resolve();
+        }
+      }
+    );
+  });
+};
